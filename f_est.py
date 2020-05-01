7621017c69a54569feae59ef2927c2d464c485d2
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2020-05-01 14:50:09 +0900

    Update f_est.py

--- f_est.py
+++ f_est.py
@@ -12,5 +12,8 @@ print(est.rsquared.astype(float).round(6))
 e=est.predict(x).astype(int)
 x=data['num']
 plt.scatter(x,y,c='red')
-plt.scatter(x,e,c='blue')
+#plt.scatter(x,e,c='blue')
+plt.scatter(x,x*71-7,c='black')
+from sklearn.metrics import r2_score
+print(r2_score(y,x*71-7))
 plt.show()
